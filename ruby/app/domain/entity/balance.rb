bd93f701f2b58429c7637e6eecaaac716a1b15e5
silva4dev <web4dev.lucas@outlook.com>
2024-11-03 22:38:20 -0300

    feat: ✨ change balance entity

--- ruby/app/domain/entity/balance.rb
+++ ruby/app/domain/entity/balance.rb
@@ -3,18 +3,19 @@
 module Domain
   module Entity
     class Balance
-      attr_reader :id, :account_id, :balance
+      attr_reader :id, :account_id, :balance, :created_at
 
       private_class_method :new
 
-      def initialize(account_id, balance)
-        @id = SecureRandom.uuid
+      def initialize(account_id, balance, id: SecureRandom.uuid, created_at: Time.now)
+        @id = id
         @account_id = account_id
         @balance = balance
+        @created_at = created_at
       end
 
-      def self.create(account_id, balance)
-        new(account_id, balance)
+      def self.create(account_id, balance, id: SecureRandom.uuid, created_at: Time.now)
+        new(account_id, balance, id: id, created_at: created_at)
       end
     end
   end
