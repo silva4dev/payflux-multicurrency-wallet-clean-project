8abb072f25b244c8283bfbf6682868631a5d8668
silva4dev <web4dev.lucas@outlook.com>
2024-11-24 19:27:07 -0300

    feat: :sparkles: add apps config

---
+++ ruby/config/apps.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Padrino.configure_apps do
+  enable :sessions
+  set :session_secret, 'aad6b2ebea3d51804462cde9e37e9f0b23f582ba76ffd9f6c1d1c838c54a1159'
+end
+
+Padrino.mount(
+  'Ruby::App',
+  :app_file => Padrino.root('app/infrastructure/controllers/app.rb'),
+  :app_class => 'Ruby::App'
+).to('/')
