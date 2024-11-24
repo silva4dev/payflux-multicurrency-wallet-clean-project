c12f4b29bfde1d61559ca388d695d1116a8c3bc4
silva4dev <web4dev.lucas@outlook.com>
2024-11-24 19:22:11 -0300

    test: :test_tube: add mock to transaction gateway

---
+++ golang/internal/usecase/mocks/transaction_gateway.go
@@ -0,0 +1,15 @@
+package mocks
+
+import (
+	"github.com.br/silva4dev/wallet-digital-microservice-project/internal/entity"
+	"github.com/stretchr/testify/mock"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
+	args := m.Called(transaction)
+	return args.Error(0)
+}
