b1b58ef7fd1900d1bc0f6ff4a4e96ae570341a00
silva4dev <web4dev.lucas@outlook.com>
2024-11-24 19:21:05 -0300

    feat: :sparkles: add create transaction use case

---
+++ golang/internal/usecase/create_transaction/create_transaction.go
@@ -0,0 +1,128 @@
+package create_transaction
+
+import (
+	"context"
+
+	"github.com.br/silva4dev/wallet-digital-microservice-project/internal/entity"
+	"github.com.br/silva4dev/wallet-digital-microservice-project/internal/gateway"
+
+	"github.com.br/silva4dev/wallet-digital-microservice-project/pkg/events"
+	"github.com.br/silva4dev/wallet-digital-microservice-project/pkg/uow"
+)
+
+type CreateTransactionInputDTO struct {
+	AccountIDFrom string  `json:"account_id_from"`
+	AccountIDTo   string  `json:"account_id_to"`
+	Amount        float64 `json:"amount"`
+}
+
+type CreateTransactionOutputDTO struct {
+	ID            string  `json:"id"`
+	AccountIDFrom string  `json:"account_id_from"`
+	AccountIDTo   string  `json:"account_id_to"`
+	Amount        float64 `json:"amount"`
+}
+
+type BalanceUpdateOutputDTO struct {
+	AccountIdFrom        string  `json:"account_id_from"`
+	AccountIdTo          string  `json:"account_id_to"`
+	BalanceAccountIdFrom float64 `json:"balance_account_id_from"`
+	BalanceAccountIdTo   float64 `json:"balance_account_id_to"`
+}
+
+type CreateTransactionUseCase struct {
+	Uow                uow.UowInterface
+	EventDispatcher    events.EventDispatcherInterface
+	TransactionCreated events.EventInterface
+	BalanceUpdated     events.EventInterface
+}
+
+func NewCreateTransactionUseCase(
+	Uow uow.UowInterface,
+	eventDispatcher events.EventDispatcherInterface,
+	transactionCreated events.EventInterface,
+	balanceUpdated events.EventInterface,
+) *CreateTransactionUseCase {
+	return &CreateTransactionUseCase{
+		Uow:                Uow,
+		EventDispatcher:    eventDispatcher,
+		TransactionCreated: transactionCreated,
+		BalanceUpdated:     balanceUpdated,
+	}
+}
+
+func (uc *CreateTransactionUseCase) Execute(ctx context.Context, input CreateTransactionInputDTO) (*CreateTransactionOutputDTO, error) {
+	output := &CreateTransactionOutputDTO{}
+	balanceUpdateOutput := &BalanceUpdateOutputDTO{}
+	err := uc.Uow.Do(ctx, func(_ *uow.Uow) error {
+		accountRepository := uc.getAccountRepository(ctx)
+		transactionRepository := uc.getTransactionRepository(ctx)
+
+		accountFrom, err := accountRepository.FindByID(input.AccountIDFrom)
+		if err != nil {
+			return err
+		}
+		accountTo, err := accountRepository.FindByID(input.AccountIDTo)
+		if err != nil {
+			return err
+		}
+		transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
+		if err != nil {
+			return err
+		}
+
+		err = accountRepository.UpdateBalance(accountFrom)
+		if err != nil {
+			return err
+		}
+
+		err = accountRepository.UpdateBalance(accountTo)
+		if err != nil {
+			return err
+		}
+
+		err = transactionRepository.Create(transaction)
+		if err != nil {
+			return err
+		}
+		output.ID = transaction.ID
+		output.AccountIDFrom = input.AccountIDFrom
+		output.AccountIDTo = input.AccountIDTo
+		output.Amount = input.Amount
+
+		balanceUpdateOutput.AccountIdFrom = input.AccountIDFrom
+		balanceUpdateOutput.AccountIdTo = input.AccountIDTo
+		balanceUpdateOutput.BalanceAccountIdFrom = accountFrom.Balance
+		balanceUpdateOutput.BalanceAccountIdTo = accountTo.Balance
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	uc.TransactionCreated.SetPayload(output)
+	uc.EventDispatcher.Dispatch(uc.TransactionCreated)
+
+	uc.BalanceUpdated.SetPayload(balanceUpdateOutput)
+	uc.EventDispatcher.Dispatch(uc.BalanceUpdated)
+
+	return output, nil
+}
+
+func (uc *CreateTransactionUseCase) getAccountRepository(ctx context.Context) gateway.AccountGateway {
+	repo, err := uc.Uow.GetRepository(ctx, "AccountDB")
+	if err != nil {
+		panic(err)
+	}
+	return repo.(gateway.AccountGateway)
+}
+
+func (uc *CreateTransactionUseCase) getTransactionRepository(ctx context.Context) gateway.TransactionGateway {
+	repo, err := uc.Uow.GetRepository(ctx, "TransactionDB")
+	if err != nil {
+		panic(err)
+	}
+	return repo.(gateway.TransactionGateway)
+}
