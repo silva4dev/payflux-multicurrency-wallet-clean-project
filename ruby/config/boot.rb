f65c741c4498edce4c3454c8c11eb82017349658
silva4dev <web4dev.lucas@outlook.com>
2024-11-24 19:27:15 -0300

    feat: :sparkles: add boot config

---
+++ ruby/config/boot.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RACK_ENV = ENV['RACK_ENV'] ||= 'development' unless defined?(RACK_ENV)
+PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
+
+require 'bundler/setup'
+Bundler.require(:default, RACK_ENV)
+
+Padrino::Logger::Config[:development][:log_level]  = :devel
+Padrino::Logger::Config[:development][:log_static] = true
+$LOAD_PATH.unshift Padrino.root('lib')
+
+Padrino.dependency_paths.unshift Padrino.root('config/initializers/*.rb')
+
+Padrino.before_load do
+  Padrino.dependency_paths << Padrino.root('app/infrastructure/controllers/**/*.rb')
+end
+
+Padrino.after_load do
+end
+
+Padrino.load!
