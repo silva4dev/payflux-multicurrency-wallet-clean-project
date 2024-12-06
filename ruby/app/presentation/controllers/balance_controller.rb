9bafecfb9175881551f67097374c826857268b64
silva4dev <web4dev.lucas@outlook.com>
2024-12-05 22:49:42 -0300

    feat: :sparkles: change balance controller

--- ruby/app/presentation/controllers/balance_controller.rb
+++ ruby/app/presentation/controllers/balance_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Ruby::App.controllers :balances, map: '/balances' do
-  get :index do
+  get '/:account_id' do
     content_type :json
     { message: 'Hello World!' }.to_json
   end
