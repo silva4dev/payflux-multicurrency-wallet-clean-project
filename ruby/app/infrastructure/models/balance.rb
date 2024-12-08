dd151270507ad5530f5398cc4b2408befacbe484
silva4dev <web4dev.lucas@outlook.com>
2024-12-08 00:14:06 -0300

    refactor: :sparkles: move files

--- ruby/app/infrastructure/models/balance.rb
+++ ruby/app/infrastructure/models/balance.rb
@@ -3,6 +3,12 @@
 module Infrastructure
   module Models
     class Balance < Sequel::Model
+      plugin :validation_helpers
+      
+      def validate
+        super
+        validates_presence %w[:account_id :balance]
+      end
     end
   end
 end
