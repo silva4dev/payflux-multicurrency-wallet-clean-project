d1d345657807efaca0c75270a5a846c2ac7e0df8
silva4dev <web4dev.lucas@outlook.com>
2025-01-12 15:59:19 -0300

    feat: :sparkles: use dotenv

--- balances/config/database.rb
+++ balances/config/database.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
+
 Sequel::Model.raise_on_save_failure = false
 
 Sequel::Model.db = case Padrino.env
 when :development
-  Sequel.connect('mysql2://root:root@balances-mysql:3306/balances', loggers: [logger])
+  Sequel.connect(ENV['DEV_DATABASE_URL'], loggers: [logger])
 when :production
-  Sequel.connect('mysql2://root:root@balances-mysql:3306/balances_production', loggers: [logger])
+  Sequel.connect(ENV['PROD_DATABASE_URL'], loggers: [logger])
 when :test
-  Sequel.connect('mysql2://root:root@balances-mysql:3306/balances_test', loggers: [logger])
+  Sequel.connect(ENV['TEST_DATABASE_URL'], loggers: [logger])
 else
   raise "Unknown environment: #{Padrino.env}"
 end
