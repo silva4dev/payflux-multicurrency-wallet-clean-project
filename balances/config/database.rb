5034f641fcc984cc4447e8bdeeb309e9d1b2bbb2
silva4dev <web4dev.lucas@outlook.com>
2025-01-12 20:23:48 -0300

    feat: :sparkles: format file correctly

--- balances/config/database.rb
+++ balances/config/database.rb
@@ -5,12 +5,12 @@ require 'dotenv/load'
 Sequel::Model.raise_on_save_failure = false
 
 Sequel::Model.db = case Padrino.env
-when :development
-  Sequel.connect(ENV['DEV_DATABASE_URL'], loggers: [logger])
-when :production
-  Sequel.connect(ENV['PROD_DATABASE_URL'], loggers: [logger])
-when :test
-  Sequel.connect(ENV['TEST_DATABASE_URL'], loggers: [logger])
-else
-  raise "Unknown environment: #{Padrino.env}"
-end
+                   when :development
+                     Sequel.connect(ENV.fetch('DEV_DATABASE_URL'), loggers: [logger])
+                   when :production
+                     Sequel.connect(ENV.fetch('PROD_DATABASE_URL'), loggers: [logger])
+                   when :test
+                     Sequel.connect(ENV.fetch('TEST_DATABASE_URL'), loggers: [logger])
+                   else
+                     raise "Unknown environment: #{Padrino.env}"
+                   end
