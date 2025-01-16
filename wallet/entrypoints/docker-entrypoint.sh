b0eaaf9dee3dec1b21beaf42143b3983596633b2
silva4dev <web4dev.lucas@outlook.com>
2025-01-16 15:44:50 -0300

    chore: :sparkles: add reflex to watch files

--- wallet/entrypoints/docker-entrypoint.sh
+++ wallet/entrypoints/docker-entrypoint.sh
@@ -4,10 +4,12 @@ set -e
 
 go mod tidy
 
+go install github.com/cespare/reflex@latest
+
 go install -tags "mysql" github.com/golang-migrate/migrate/v4/cmd/migrate@v4.14.1
 
 migrate -path ./db/migrate -database "mysql://root:root@tcp(wallet-mysql:3306)/wallet?parseTime=true" up
 
 go run db/seeds.go
 
-go run cmd/walletcore/main.go
+reflex -c reflex.conf
