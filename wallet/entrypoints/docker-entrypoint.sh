c6186eec7f225ab188ae997eaaf339528bf1c7d6
silva4dev <web4dev.lucas@outlook.com>
2025-01-16 13:06:22 -0300

    feat: :sparkles: add configs to run migrations

--- wallet/entrypoints/docker-entrypoint.sh
+++ wallet/entrypoints/docker-entrypoint.sh
@@ -4,4 +4,10 @@ set -e
 
 go mod tidy
 
+go install -tags "mysql" github.com/golang-migrate/migrate/v4/cmd/migrate@v4.14.1
+
+migrate -path ./db/migrate -database "mysql://root:root@tcp(wallet-mysql:3306)/wallet?parseTime=true" up
+
+go run db/seeds.go
+
 go run cmd/walletcore/main.go
