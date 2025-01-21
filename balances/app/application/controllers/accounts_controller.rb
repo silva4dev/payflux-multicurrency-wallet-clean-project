0eac0f201467582ac12d1713e2f55ea0e75d0b63
silva4dev <web4dev.lucas@outlook.com>
2025-01-21 00:51:40 -0300

    refactor: :sparkles: rename file

--- balances/app/application/controllers/accounts_controller.rb
+++ balances/app/application/controllers/accounts_controller.rb
@@ -4,22 +4,22 @@ require_relative '../dtos/find_account_dto'
 require_relative '../usecases/find_account_usecase'
 require_relative '../../infrastructure/repositories/balance_repository'
 
-Ruby::App.controllers :balances, map: '/balances' do
+Ruby::App.controllers :accounts, map: '/accounts' do
   get '/:account_id' do
     content_type :json
     input_dto = Application::Dtos::FindAccountDto::InputDTO.new(account_id: params[:account_id])
     repository = Infrastructure::Repositories::BalanceRepository.new
     usecase = Application::Usecases::FindAccountUsecase.new(repository)
-    balance = usecase.execute(input_dto)
-    if balance.nil?
+    account = usecase.execute(input_dto)
+    if account.nil?
       status 404
-      { error: 'Balance not found' }.to_json
+      { error: 'Account not found' }.to_json
     else
       output_dto = Application::Dtos::FindAccountDto::OutputDTO.new(
-        id: balance.id,
-        account_id: balance.account_id, 
-        balance: balance.balance, 
-        created_at: balance.created_at
+        id: account.id,
+        account_id: account.account_id, 
+        balance: account.balance, 
+        created_at: account.created_at
       ).to_h
       output_dto.to_json
     end
